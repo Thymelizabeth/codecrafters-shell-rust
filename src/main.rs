20e55fbd21e512f2c314e6ef6fe8980d2f3e3fc7
Elizabeth Doughty <thymelizabeth@thymelizabeth.nz>
2026-01-29 23:20:38 +1300

    Refactor displaying prompt to separate function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::io::{self, Write};
 
 fn main() -> Result<(), io::Error> {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    prompt()?;
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
     println!("{}: command not found", input.trim());
     Ok(())
+}
+
+fn prompt() -> Result<(), io::Error> {
+    let mut stdout = io::stdout();
+    write!(&mut stdout, "$ ")?;
+    stdout.flush();
 }
