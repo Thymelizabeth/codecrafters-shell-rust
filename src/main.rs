eb72f440e4c870e56b10652dc7510fabbce64949
Elizabeth Doughty <thymelizabeth@thymelizabeth.nz>
2026-01-29 23:36:33 +1300

    Add Command type, with associated function parse_command. Refactor to use Command::parse_command.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::io::{self, Write};
 
+enum Command<'a> {
+    Unknown(&'a str),
+}
+
 fn main() -> Result<(), io::Error> {
     let mut input = String::new();
     loop {
         prompt()?;
 
         io::stdin().read_line(&mut input)?;
-        println!("{}: command not found", input.trim());
+        match Command::parse_command(&input) {
+            Command::Unknown(cmd) => println!("{}: command not found", cmd.trim()),
+        }
         input.clear();
     }
     Ok(())
@@ -16,4 +22,12 @@ fn prompt() -> Result<(), io::Error> {
     let mut stdout = io::stdout();
     write!(&mut stdout, "$ ")?;
     stdout.flush()
+}
+
+impl<'a> Command<'a> {
+    fn parse_command(input: &'a str) -> Self {
+        match input.trim() {
+            _ => Command::Unknown(input),
+        }
+    }
 }
