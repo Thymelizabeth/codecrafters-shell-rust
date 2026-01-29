bbb111cdf75775c67a6e409710618bf637193e68
Elizabeth Doughty <thymelizabeth@thymelizabeth.nz>
2026-01-29 23:05:55 +1300

    Use concrete io::Error instead of dyn

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::{self, Write};
 
-fn main() -> Result<(), Box<dyn io::Error>> {
+fn main() -> Result<(), Box<io::Error>> {
     print!("$ ");
     io::stdout().flush().unwrap();
     let mut input = String::new();
