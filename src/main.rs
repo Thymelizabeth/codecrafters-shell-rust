9deba4190aaebd6e87ba814c0e6b4ab3afbc655f
Elizabeth Doughty <thymelizabeth@thymelizabeth.nz>
2026-01-30 01:44:19 +1300

    Add Command::Executable

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use std::io::{self, Write};
+use std::{
+    env,
+    fs::{self, DirEntry},
+    io::{self, Write},
+    os::unix::fs::PermissionsExt,
+    path::Path,
+};
 
 enum Command<'a> {
     Builtin(Builtin<'a>),
+    Executable(DirEntry, &'a str),
     Unknown(&'a str),
 }
 
@@ -15,11 +22,11 @@ fn main() -> Result<(), io::Error> {
     let mut input = String::new();
     loop {
         prompt()?;
-
         io::stdin().read_line(&mut input)?;
         match Command::from(input.as_str()) {
             Command::Builtin(Builtin::Exit) => break,
             Command::Builtin(cmd) => cmd.eval()?,
+            Command::Executable(_cmd, _args) => todo!(),
             Command::Unknown(cmd) => writeln!(io::stdout(), "{}: command not found", cmd.trim())?,
         }
         input.clear();
@@ -43,6 +50,26 @@ impl<'a> From<&'a str> for Command<'a> {
             Some("echo") => Command::Builtin(Builtin::Echo(args)),
             Some("type") => Command::Builtin(Builtin::Type(args)),
             _ => Command::Unknown(input),
+            Some(cmd) => {
+                let path = env::var_os("PATH").unwrap_or_default();
+                let path = env::split_paths(&path);
+                for dir in path {
+                    if let Ok(mut dir_contents) = fs::read_dir(dir)
+                        && let Some(cmd) = dir_contents.find_map(|file| {
+                            let file = file.ok()?;
+                            file.file_name()
+                                .to_str()
+                                .is_some_and(|name| name == cmd)
+                                .then_some(file)
+                        })
+                        && is_executable(&cmd.path())
+                    {
+                        return Command::Executable(cmd, args);
+                    }
+                }
+                Command::Unknown(input)
+            }
+            None => Command::Unknown(input),
         }
     }
 }
@@ -54,9 +81,24 @@ impl<'a> Builtin<'a> {
             Builtin::Echo(args) => writeln!(io::stdout(), "{}", args.trim())?,
             Builtin::Type(arg) => match Command::from(arg) {
                 Command::Builtin(_) => writeln!(io::stdout(), "{} is a shell builtin", arg.trim())?,
+                Command::Executable(cmd, _) => writeln!(
+                    io::stdout(),
+                    "{} is {}",
+                    cmd.file_name().display(),
+                    cmd.path().display()
+                )?,
                 Command::Unknown(_) => writeln!(io::stdout(), "{}: not found", arg.trim())?,
             },
         }
         Ok(())
     }
+}
+
+#[cfg(unix)]
+fn is_executable(path: &Path) -> bool {
+    let metadata = match path.metadata() {
+        Ok(metadata) => metadata,
+        Err(_) => return false,
+    };
+    metadata.is_file() && metadata.permissions().mode() & 0o111 != 0
 }
