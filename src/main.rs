8cddfbb9bb43814086e87026a7dc48ea371d9a5a
Elizabeth Doughty <thymelizabeth@thymelizabeth.nz>
2026-01-29 23:04:55 +1300

    Print error message on invalid command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::io::{self, Write};
 
-fn main() {
+fn main() -> Result<(), Box<dyn io::Error>> {
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    println!("{}: command not found", input.trim());
+    Ok(())
 }
